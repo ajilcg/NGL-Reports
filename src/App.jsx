7d83c822660a5ad7083ea63bb7ddf5d5c58e5ff5
ajilcg <ajilc50@gmail.com>
2024-11-12 14:37:58 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import * as React from 'react';
+import Layout from './Component/Layout';
+ 
+
+function App() {
+ 
+  return (
+    <>
+  <Layout></Layout>
+    </>
+  )
+}
+
+export default App
